73872f159426a0a6a47dfc4e9b4740e4727837ec
Archontis <77233507+ArchontisKostis@users.noreply.github.com>
2022-04-14 20:13:51 +0300

    changed app.js

--- InteractiveRatingComponent/app.js
+++ InteractiveRatingComponent/app.js
@@ -1,14 +1,32 @@
 const submitButton = document.getElementById("submit_button");
-
 const choicesButtons = document.getElementsByClassName("choice");
+var choiceValue = undefined;
 
 submitButton.onclick = function (evt) {
-    this.innerHTML = "CLICKED!";
+    console.log(`CLICKED!\nValue: ${choiceValue}`);
 }
 
+choicesButtons[0].onclick = function (evt) {
+    this.focus();
+    choiceValue = 1;
+}
 
-for(var i=0; i<choicesButtons.length; i++){
-    choicesButtons[i].onclick = function (evt) {
-        this.focus();
-    }
+choicesButtons[1].onclick = function (evt) {
+    this.focus();
+    choiceValue = 2;
+}
+
+choicesButtons[2].onclick = function (evt) {
+    this.focus();
+    choiceValue = 3;
+}
+
+choicesButtons[3].onclick = function (evt) {
+    this.focus();
+    choiceValue = 4;
+}
+
+choicesButtons[4].onclick = function (evt) {
+    this.focus();
+    choiceValue = 5;
 }
